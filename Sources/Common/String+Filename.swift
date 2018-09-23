a9fed45a96fc915377d1bffbc52a4ffba28493c3
Kojirou <Kojirouhtc@gmail.com>
2018-09-23 18:24:40 +0800

    fix a filename issue

--- Sources/Common/String+Filename.swift
+++ Sources/Common/String+Filename.swift
@@ -11,7 +11,7 @@ import Foundation
 extension String {
     
     public var filenameWithoutExtension: String {
-        return lastPathComponent.deletingPathExtension
+        return deletingPathExtension.lastPathComponent
     }
     
     public var filename: String {
