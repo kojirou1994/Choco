fbc24206fe47de7f99b19295eea176fd88739c78
kojirou <kojirouhtc@gmail.com>
2022-05-25 11:48:06 +0800

    choco-cli: add endless mode

--- Sources/choco-cli/Commands/MuxFile.swift
+++ Sources/choco-cli/Commands/MuxFile.swift
@@ -4,6 +4,9 @@ import Foundation
 
 struct MuxFile: ParsableCommand {
 
+  @Flag(help: "Endless loop muxing if any success file")
+  var endless: Bool = false
+
   @Flag(help: "Recursive into directories.")
   var recursive: Bool = false
 
@@ -26,41 +29,58 @@ struct MuxFile: ParsableCommand {
     try common.withMuxerSetup { muxer in
       let fileRemuxOptions = FileRemuxOptions(recursive: recursive, copyNormalFiles: copyNormalFiles, copyOverwrite: copyOverwrite, removeSourceFiles: removeSourceFiles, fileTypes: FileRemuxOptions.defaultFileTypes)
 
-      let results = inputs.map { input in
-        muxer.mux(file: URL(fileURLWithPath: input), options: fileRemuxOptions)
-      }
+      var endlessLoopCount = 0
 
-      print("\n\nSummary:\n")
-      print("================")
-      for (input, result) in zip(inputs, results) {
-        defer {
-          print("================\n")
+      while true {
+
+        let results = inputs.map { input in
+          muxer.mux(file: URL(fileURLWithPath: input), options: fileRemuxOptions)
         }
-        print("Input: \(input)")
-        switch result {
-        case .success(let summary):
-          print("Read input OK.")
-          print()
-          print("Handled media files:\n")
-          if summary.files.isEmpty {
-            print("None")
-          } else {
-            summary.files.forEach { fileTask in
-              print("\(fileTask.input.path.path) \(ByteCountFormatter.string(fromByteCount: numericCast(fileTask.input.size), countStyle: .file))")
-              switch fileTask.output {
-              case .success(let output):
-                print("success ==>")
-                print("\(output.path.path) \(ByteCountFormatter.string(fromByteCount: numericCast(output.size), countStyle: .file))")
-              case .failure(let error):
-                print("failure: \(error)")
-              }
-              print()
-            }
+
+        print("\n\nSummary:\n")
+        print("================")
+
+        var hasSuccessFileTask = false
+
+        for (input, result) in zip(inputs, results) {
+          defer {
+            print("================\n")
           }
-        case .failure(let error):
-          print("Read input failed: \(error)")
+          print("Input: \(input)")
+          switch result {
+          case .success(let summary):
+            print("Read input OK.")
+            print()
+            print("Handled media files:\n")
+            if summary.files.isEmpty {
+              print("None")
+            } else {
+              summary.files.forEach { fileTask in
+                print("\(fileTask.input.path.path) \(ByteCountFormatter.string(fromByteCount: numericCast(fileTask.input.size), countStyle: .file))")
+                switch fileTask.output {
+                case .success(let output):
+                  hasSuccessFileTask = true
+                  print("success ==>")
+                  print("\(output.path.path) \(ByteCountFormatter.string(fromByteCount: numericCast(output.size), countStyle: .file))")
+                case .failure(let error):
+                  print("failure: \(error)")
+                }
+                print()
+              }
+            }
+          case .failure(let error):
+            print("Read input failed: \(error)")
+          }
         }
-      }
+
+        if endless, hasSuccessFileTask {
+          endlessLoopCount += 1
+          print("starting endless loop \(endlessLoopCount)")
+        } else {
+          break
+        }
+
+      } // loop end
     }
 
   }
