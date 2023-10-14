15ef7407bbc7a9e36b9355f8f692fb6ad55d920c
kojirou <kojirouhtc@gmail.com>
2023-10-14 11:45:27 +0800

    better vs crop templating

--- Sources/libChoco/VapourSynth/template.swift
+++ Sources/libChoco/VapourSynth/template.swift
@@ -6,15 +6,32 @@ public func generateScript(encodeScript: String, filePath: String, trackIndex: I
 //  let template = try String(contentsOfFile: templatePath)
   let parser = MustacheParser()
   let tree = parser.parse(string: encodeScript)
-  let dic = [
+  var dic = [
     "filePath": "r\"" + filePath + "\"",
 //    "trackIndex": trackIndex,
-//    "cropLeft": cropInfo.left,
-//    "cropRight": cropInfo.right,
-//    "cropTop": cropInfo.top,
-//    "cropBottom": cropInfo.bottom,
     "encoderDepth": encoderDepth
   ] as [String : Any]
+
+  /*
+   if {{{cropLeft}}} == "":
+     src = core.std.CropAbs(src, {{{cropWidth}}}, {{{cropHeight}}}, {{{cropLeft}}}, {{{cropTop}}})
+   else:
+     src = core.std.Crop(src, {{{cropLeft}}}, {{{cropRight}}}, {{{cropTop}}}, {{{cropBottom}}})
+   */
+  switch cropInfo {
+  case .relative(let top, let bottom, let left, let right):
+    dic["cropLeft"] = left
+    dic["cropRight"] = right
+    dic["cropTop"] = top
+    dic["cropBottom"] = bottom
+  case .absolute(let width, let height, let x, let y):
+    dic["cropWidth"] = width
+    dic["cropHeight"] = height
+    dic["cropLeft"] = x
+    dic["cropTop"] = y
+  case nil:
+    break
+  }
   let result = tree.render(object: dic)
   return result
 }
