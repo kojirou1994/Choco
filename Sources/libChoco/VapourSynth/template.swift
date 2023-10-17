a9602a598bf9b13d4e737f50e201c5a1fc682de0
kojirou <kojirouhtc@gmail.com>
2023-10-17 11:01:23 +0800

    optimize crop template

--- Sources/libChoco/VapourSynth/template.swift
+++ Sources/libChoco/VapourSynth/template.swift
@@ -10,34 +10,46 @@ public func generateScript(encodeScript: String, filePath: String, trackIndex: I
     "filePath": "r\"" + filePath + "\"",
     "filePath_WIN": "r\"" + filePath.replacingOccurrences(of: "/", with: "\\") + "\"",
 //    "trackIndex": trackIndex,
+    // default crop props, make py happy
+    "cropTop": 0,
+    "cropBottom": 0,
+    "cropWidth": 0,
+    "cropHeight": 0,
+    "cropLeft": 0,
+    "cropRight": 0,
     "encoderDepth": encoderDepth
   ] as [String : Any]
 
   /*
    if "{{{cropMode}}}" == "relative":
      src = core.std.Crop(src, {{{cropLeft}}}, {{{cropRight}}}, {{{cropTop}}}, {{{cropBottom}}})
-   else:
+   elif "{{{cropMode}}}" == "absolute":
      src = core.std.CropAbs(src, {{{cropWidth}}}, {{{cropHeight}}}, {{{cropLeft}}}, {{{cropTop}}})
    */
+
+  enum CropMode: String, CustomStringConvertible {
+    case none
+    case relative
+    case absolute
+
+    var description: String { rawValue }
+  }
+
   switch cropInfo {
   case .relative(let top, let bottom, let left, let right):
-    dic["cropMode"] = "relative"
-    dic["cropWidth"] = "0"
-    dic["cropHeight"] = "0"
+    dic["cropMode"] = CropMode.relative
     dic["cropLeft"] = left
     dic["cropRight"] = right
     dic["cropTop"] = top
     dic["cropBottom"] = bottom
   case .absolute(let width, let height, let x, let y):
-    dic["cropMode"] = "absolute"
-    dic["cropRight"] = "0"
-    dic["cropBottom"] = "0"
+    dic["cropMode"] = CropMode.absolute
     dic["cropWidth"] = width
     dic["cropHeight"] = height
     dic["cropLeft"] = x
     dic["cropTop"] = y
   case nil:
-    break
+    dic["cropMode"] = CropMode.none
   }
   let result = tree.render(object: dic)
   return result
