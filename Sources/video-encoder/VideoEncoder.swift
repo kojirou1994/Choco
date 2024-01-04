be363680fecde4fc1cfbd0c19bffdf6fa03b9a25
kojirou <kojirouhtc@gmail.com>
2024-01-05 04:12:05 +0800

    update

--- Sources/video-encoder/VideoEncoder.swift
+++ Sources/video-encoder/VideoEncoder.swift
@@ -6,6 +6,12 @@ import SystemUp
 @main
 struct VideoEncoder: ParsableCommand {
 
+  @Flag
+  var overwrite: Bool = false
+
+  @Flag
+  var removeInput: Bool = false
+
   @Option(name: .shortAndLong)
   var encoder: String
 
@@ -22,6 +28,17 @@ struct VideoEncoder: ParsableCommand {
   var params: [String]
 
   func run() throws {
+    var status = FileStatus()
+
+    if overwrite {
+      _ = FileSyscalls.unlink(.absolute(.init(output)))
+    } else {
+      switch FileSyscalls.fileStatus(.absolute(.init(output)), into: &status) {
+      case .success: throw Errno.fileExists
+      case .failure: break
+      }
+    }
+
     print("encoder", encoder)
     print("input", input)
     print("output", output)
@@ -46,8 +63,19 @@ struct VideoEncoder: ParsableCommand {
       command.stdin = .path(path, mode: .readOnly, options: [])
     }
     command.cwd = cwd
-    let output = try command.output()
-    throw ExitCode(output.status.exitStatus)
+    let statusCode = try command.output().status.exitStatus
+    if statusCode != 0 {
+      throw ExitCode(statusCode)
+    }
+    switch FileSyscalls.fileStatus(.absolute(.init(output)), into: &status) {
+    case .success:
+      if removeInput {
+        print("encode succeess, remove input")
+        _ = FileSyscalls.unlink(.absolute(.init(input)))
+      }
+    case .failure:
+      print("encoder exit 0 but output not existed!")
+    }
   }
 
 }
