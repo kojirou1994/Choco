f87b2e701a53167ebf2a8d07891bbaabeb59ac09
kojirou <kojirouhtc@gmail.com>
2024-06-08 00:19:03 +0800

    lock swift-argument-parser version
    1.4.0 seems not good

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     .package(url: "https://github.com/kojirou1994/Precondition.git", from: "1.0.0"),
     .package(url: "https://github.com/kojirou1994/Units.git", from: "0.0.1"),
     .package(url: "https://github.com/kojirou1994/PrettyBytes.git", from: "0.0.1"),
-    .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.0.0"),
+    .package(url: "https://github.com/apple/swift-argument-parser.git", exact: "1.3.1"),
     .package(url: "https://github.com/kojirou1994/URLFileManager.git", from: "0.0.1"),
     .package(url: "https://github.com/kojirou1994/MediaUtility.git", from: "0.6.0"),
     .package(url: "https://github.com/kojirou1994/Executable.git", from: "0.6.0"),
